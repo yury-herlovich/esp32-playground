a7c3b122d4c626be440cd5f71fb394516335eb19
Yury Herlovich <yuryherlovich@gmail.com>
2024-12-22 15:50:50 -0700

    wifi connection using wifiMulti

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,31 @@
 #include <Arduino.h>
+#include <WiFiMulti.h>
+#include <secrets.h>
+
+WiFiMulti wifiMulti;
 
 void blink(int time);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-
   Serial.begin(921600);
+
   Serial.println("Hello from the setup");
+
+  // connect to wifi
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASS);
+
+  blink(250);
+
+  while (wifiMulti.run() != WL_CONNECTED) {
+    blink(250);
+  }
+
+  Serial.println("Connected to wifi");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  blink(250);
-  blink(250);
-
-  blink(1000);
-  Serial.println("Hello from the loop");
+  digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED ? HIGH : LOW);
 }
 
 void blink(int time) {
